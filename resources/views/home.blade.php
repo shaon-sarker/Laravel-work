dd323bf645d4b11848fe053be4b809a1a28f0fc9
shaon-sarker <shaon180054+github@diit.info>
2021-09-06 18:14:00 +0600

    "admin template ingration"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
-@extends('layouts.app')
-
+@extends('layouts.dashboad')
+@section('dashboard')
+active
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@include('layouts.nav')
+<!-- ########## START: MAIN PANEL ########## -->
+<div class="sl-mainpanel">
+    <nav class="breadcrumb sl-breadcrumb">
+      <a class="breadcrumb-item" href="index.html">Starlight</a>
+      <a class="breadcrumb-item" href="index.html">Pages</a>
+      <span class="breadcrumb-item active">Blank Page</span>
+    </nav>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="sl-pagebody">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
